3eef8f6040edcc9a0c9e4c42dee6ce175e01bac0
GUT-9 <futianji0702@gmail.com>
2025-10-31 09:06:25 +0800

    调用API

---
+++ src/main/java/org/example/DeepSeekRequestModel.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+
+/**
+ * @author hxy
+ */
+@Data
+@Builder
+public class DeepSeekRequestModel {
+    /**
+     * 所用DeepSeek模型
+     */
+    private String model;
+    private List<Message> messages;
+
+    /**
+     * 消息体
+     */
+    @Data
+    @Builder
+    public static class Message {
+        private String role;
+        private String content;
+    }
+}
